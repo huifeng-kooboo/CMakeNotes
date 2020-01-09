f9ff826d807bebc4e3c74dd7c911803b1b40f001
huifeng-kooboo <gisdoing@gmail.com>
2020-01-09 11:27:35 +0800

    添加HelloWorld文件

---
+++ HelloWorld/HelloWorld.cpp
@@ -0,0 +1,18 @@
+// HelloWorld.cpp : 定义控制台应用程序的入口点。
+//
+
+#include <iostream>
+#include <string>
+
+void HelloWorld()
+{
+	std::cout << "Hello World" << std::endl;
+}
+
+int main(int argc,char* argv[])
+{
+	HelloWorld();
+	system("pause");
+	return 0;
+}
+
